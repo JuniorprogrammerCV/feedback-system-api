d8314abc3345162fae37cf8cb9ba864cca24c308
Junior Delgado <juprogramador@gmail.com>
2024-11-22 19:37:36 -0100

    defining the User model using sequelize, including the fields and their constraints

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+//feedback-system\src\models\User.js
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'client'),
+    defaultValue: 'client'
+  }
+});
+
+module.exports = User;
