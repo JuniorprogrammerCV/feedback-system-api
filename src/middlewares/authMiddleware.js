a86361e9e704e8da16625952bd97657775fbabe5
Junior Delgado <juprogramador@gmail.com>
2024-11-22 19:30:39 -0100

    middleware to verify user authentication and whether the user has administrator permission

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+//feedback-system\src\middlewares\authMiddleware.js
+
+const passport = require('passport');
+
+exports.checkAuth = (req, res, next) => {
+  passport.authenticate('local', { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+exports.checkAdmin = (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+  next();
+};
